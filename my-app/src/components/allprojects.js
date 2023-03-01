244e1a67f3df76745ff72e17ca110c8b1e8006ef
amehta0104 <45803380+amehta0104@users.noreply.github.com>
2023-03-01 01:27:53 -0500

    Update allprojects.js

--- my-app/src/components/allprojects.js
+++ my-app/src/components/allprojects.js
@@ -126,7 +126,7 @@ function AllProjects() {
                           </Grid>
                           <Grid item xs={12} sm={12} md={12} >
                          <Typography variant="body2" paragraph  gutterBottom>
-                          lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                          gg
                         </Typography>
                           </Grid>
           {data.map((project) => (
